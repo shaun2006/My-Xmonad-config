d67016bd0f880d906457100cb5c57cbb3370f37c
Shaun2006 <116490490+shaun2006@users.noreply.github.com>
2026-04-19 19:39:13 +0530

    fixing xrander

--- install_script.sh
+++ install_script.sh
@@ -1,4 +1,4 @@
-sudo pacman -S git xmonad xmonad-contrib rofi kitty xdotool sddm alsa-utils pipewire pavucontrol system-config-printer redshift fastfetch xorg firefox ttf-ubuntu-font-family ttf-mononoki-nerd otf-font-awesome thunar feh okular vlc gimp screenkey xrandr qalculate-gtk flameshot thunderbird lxappearance qt5ct qt6ct breeze breeze-gtk
+sudo pacman -S git xmonad xmonad-contrib rofi kitty xdotool sddm alsa-utils pipewire pavucontrol system-config-printer redshift fastfetch xorg firefox ttf-ubuntu-font-family ttf-mononoki-nerd otf-font-awesome thunar feh okular vlc gimp screenkey xorg-xrandr qalculate-gtk flameshot thunderbird lxappearance qt5ct qt6ct breeze breeze-gtk
 git clone https://github.com/shaun2006/My-Xmonad-config.git
 cd My-Xmonad-config
 mkdir -p ~/.config/xmonad ~/.config/xmobar ~/Pictures/
